fdb947d0f4fe65d7637c2a5042406795b615131f
jake <panwujiez@163.com>
2015-03-10 17:11:07 +0800

    detail

--- lib/zhaoapi.pm
+++ lib/zhaoapi.pm
@@ -103,7 +103,7 @@ any '/sdk/list' => sub {
                       'status' => $promotions->{12}->{status},
                       'point' => $promotions->{12}->{'expenses'} * $publisher_margin,
                       'conditions' => '搜索关键字“旅游”，下载第四个APP携程旅游体验满三分钟并在期间完成注册即可获取奖励',
-                      'detial' => '酒店、机票、火车票、门票、目的地攻略、旅游、语音查询，团购，一个都不能少。手机查询预订更方便，机票、酒店、门票返现更给力。无限旅程，尽在携程！',
+                      'detail' => '酒店、机票、火车票、门票、目的地攻略、旅游、语音查询，团购，一个都不能少。手机查询预订更方便，机票、酒店、门票返现更给力。无限旅程，尽在携程！',
                       'jumpUrl' => 'http://www.baidu.com',
                   },
                   {
@@ -113,7 +113,7 @@ any '/sdk/list' => sub {
                       'status' => $promotions->{13}->{status},
                       'point' => $promotions->{13}->{'expenses'} * $publisher_margin,
                       'conditions' => '首次下载联网注册成功试玩即可获取奖励',
-                      'detial' => '有人的地方便有江湖，《大掌门》即是基于玩家心中对武侠梦的向往而诞生的武侠风策略RPG精品卡牌手机端网游，游戏中玩家扮演一位武学宗师，统领整个门派，拥有各样神功！',
+                      'detail' => '有人的地方便有江湖，《大掌门》即是基于玩家心中对武侠梦的向往而诞生的武侠风策略RPG精品卡牌手机端网游，游戏中玩家扮演一位武学宗师，统领整个门派，拥有各样神功！',
                       'jumpUrl' => 'http://www.sina.com',
                   },
                   {
@@ -123,7 +123,7 @@ any '/sdk/list' => sub {
                       'status' => $promotions->{14}->{status},
                       'point' => $promotions->{14}->{'expenses'} * $publisher_margin,
                       'conditions' => '首次下载联网注册成功试玩即可获取奖励',
-                      'detial' => '国内第一款可以真正支持语音的德州扑克，让你畅聊、畅玩、High到爆',
+                      'detail' => '国内第一款可以真正支持语音的德州扑克，让你畅聊、畅玩、High到爆',
                       'jumpUrl' => 'http://www.weibo.com',
                   },
                   ],
